0d940635cc79389cd8ca388499e6caf26029faed
wangaixia <fjpengpeng@163.com>
2016-04-17 00:26:05 +0800

    v2

--- tar.py
+++ tar.py
@@ -2,7 +2,7 @@ import os
 import tarfile
 
 os.chdir('/tmp')
-for fike in os.walk
-tar = tarfile.open("catalina-2016-04-10.out.tar.gz","w:gz")
-tar.add("catalina-2016-04-10.out")
+for file in os.walk("/tmp"):
+    tar = tarfile.open("file.tar.gz", "w:gz")
+    tar.add("catalina-2016-04-10.out")
 tar.close()
